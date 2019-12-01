fe2feaa4700c17359900eb2b5b1cff955452b743
Amy Franz <amy.franz@gmail.com>
2019-12-01 17:34:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   var newArray = [];
+  newArray.length = musicians.length;
   for (var i = 1; i < Array.length(musicians); i++) {
-    newArray[i] = 
+    newArray[i] = `${musicians[i]} plays ${
   }
 }
